191dbbbd8ded2594c4041d7beccb7d580c6bb102
jefersongjr <jeferson_gjr@hotmail.com>
2022-03-22 15:54:14 -0300

    desafio 3, criando o Input

--- script.js
+++ script.js
@@ -7,11 +7,29 @@ function createHeader() {
 
 createHeader();
 
+const header1 = document.getElementsByTagName('header')[0];
 function createH1() {
-  const header1 = document.getElementsByTagName('header')[0];
   const h1 = document.createElement('h1');
   h1.innerText = 'Minha Lista de Tarefas';
   header1.appendChild(h1);
 }
 
 createH1();
+
+function createP() {
+  const p = document.createElement('p');
+  p.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+  p.id = 'funcionamento';
+  p.style.color = 'gray';
+  header1.appendChild(p);
+}
+
+createP();
+
+function createInput() {
+  const input = document.createElement('input');
+  input.id = 'texto-tarefa';
+  header1.appendChild(input);
+}
+
+createInput();
