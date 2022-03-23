e51c989d983a9631ce065ef5467c3e89288c611d
jefersongjr <jeferson_gjr@hotmail.com>
2022-03-23 12:03:53 -0300

    criando lis da lista

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+let list = {
+  task: '',
+};
 
+const ol = document.getElementById('lista-tarefas');
+const buttonAdd = document.querySelector('#criar-tarefa');
 
+function createLi(object) {
+  const li = document.createElement('li');
 
+  li.innerText = object.task;
+
+  ol.appendChild(li);
+}
+
+function addTask() {
+  list.task = document.getElementById('texto-tarefa').value;
+
+  localStorage.setItem(list.task, JSON.stringify(list));
+
+  document.getElementById('texto-tarefa').value = '';
+
+  createLi(list);
+}
+
+buttonAdd.addEventListener('click', addTask);
